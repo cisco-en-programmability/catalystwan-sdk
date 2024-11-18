0c859dcbc122e54099bd06b3421c0e36c570577c
Szymon Basan <sbasan@cisco.com>
2024-11-18 17:31:19 +0100

    replace old ewpo url

--- endpoints-md.py
+++ endpoints-md.py
@@ -17,7 +17,7 @@ from catalystwan import PACKAGE_VERSION, __package__
 from catalystwan.endpoints import BASE_PATH, APIEndpointRequestMeta, TypeSpecifier, request, versions, view
 from catalystwan.utils.session_type import SessionType  # type: ignore
 
-SOURCE_BASE_PATH = "https://github.com/cisco-open/cisco-catalyst-wan-sdk/blob/main/"
+SOURCE_BASE_PATH = "https://github.com/cisco-en-programmability/catalystwan-sdk/blob/main/"
 
 
 def relative(absolute: str) -> str:
