f95e9aae4313cc21b2298c7e433e462ac59b5bac
Szymon Basan <sbasan@cisco.com>
2025-02-03 16:25:20 +0100

    fix advanced malware protection parcel

--- catalystwan/models/configuration/feature_profile/sdwan/policy_object/security/amp.py
+++ catalystwan/models/configuration/feature_profile/sdwan/policy_object/security/amp.py
@@ -4,7 +4,7 @@ from typing import List, Literal, Optional
 
 from pydantic import AliasPath, ConfigDict, Field
 
-from catalystwan.api.configuration_groups.parcel import Global, _ParcelBase, as_global
+from catalystwan.api.configuration_groups.parcel import Global, _ParcelBase
 
 FileReputationServer = Literal["nam", "eur", "apjc"]
 FileReputationAlert = Literal["critical", "warning", "info"]
@@ -22,8 +22,8 @@ class AdvancedMalwareProtectionParcel(_ParcelBase):
         default="unified/advanced-malware-protection", exclude=True
     )
     description: str = "advancedMalwareProtection"
-    match_all_vpn: Global[bool] = Field(
-        default=Global[bool](value=True), validation_alias=AliasPath("data", "matchAllVpn")
+    match_all_vpn: Global[Literal[True]] = Field(
+        default=Global[Literal[True]](value=True), validation_alias=AliasPath("data", "matchAllVpn")
     )
     file_reputation_cloud_server: Global[FileReputationServer] = Field(
         validation_alias=AliasPath("data", "fileReputationCloudServer")
@@ -55,17 +55,15 @@ class AdvancedMalwareProtectionParcel(_ParcelBase):
         file_reputation_cloud_server: FileReputationServer,
         file_reputation_est_server: FileReputationServer,
         file_reputation_alert: FileReputationAlert,
-        match_all_vpn: bool,
         file_analysis_enabled: bool = False,
         file_analysis_alert: Optional[FileAnalysisAlert] = None,
         file_analysis_cloud_server: Optional[FileAnalysisServer] = None,
         file_analysis_file_types: List[FileAnalysisFileTypes] = [],
     ):
-        _file_analysis_alert = as_global(file_analysis_alert, FileAnalysisAlert) if file_analysis_alert else None
+        _file_analysis_alert = Global[FileAnalysisAlert](value=file_analysis_alert) if file_analysis_alert else None
         _file_analysis_cloud_server = (
             Global[FileAnalysisServer](value=file_analysis_cloud_server) if file_analysis_cloud_server else None
         )
-
         _file_analysis_file_types = (
             Global[List[FileAnalysisFileTypes]](value=file_analysis_file_types) if file_analysis_file_types else None
         )
@@ -73,12 +71,11 @@ class AdvancedMalwareProtectionParcel(_ParcelBase):
         return cls(
             parcel_name=parcel_name,
             parcel_description=parcel_description,
-            file_reputation_cloud_server=as_global(file_reputation_cloud_server, FileReputationServer),
-            file_reputation_est_server=as_global(file_reputation_est_server, FileReputationServer),
+            file_reputation_cloud_server=Global[FileReputationServer](value=file_reputation_cloud_server),
+            file_reputation_est_server=Global[FileReputationServer](value=file_reputation_est_server),
             file_analysis_alert=_file_analysis_alert,
             file_analysis_cloud_server=_file_analysis_cloud_server,
-            file_reputation_alert=as_global(file_reputation_alert, FileReputationAlert),
-            match_all_vpn=as_global(match_all_vpn),
-            file_analysis_enabled=as_global(file_analysis_enabled),
+            file_reputation_alert=Global[FileReputationAlert](value=file_reputation_alert),
+            file_analysis_enabled=Global[bool](value=file_analysis_enabled),
             file_analysis_file_types=_file_analysis_file_types,
         )
