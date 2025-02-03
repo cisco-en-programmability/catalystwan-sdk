c8c8157aff7becb9176b7df32d346050629f73be
Szymon Basan <sbasan@cisco.com>
2025-02-03 12:15:11 +0100

    fix scalable group tag

--- catalystwan/models/configuration/feature_profile/sdwan/policy_object/security/scalable_group_tag.py
+++ catalystwan/models/configuration/feature_profile/sdwan/policy_object/security/scalable_group_tag.py
@@ -1,24 +1,24 @@
 # Copyright 2024 Cisco Systems, Inc. and its affiliates
-from typing import List, Literal, Optional
+from typing import List, Literal
 
 from pydantic import AliasPath, Field
 
-from catalystwan.api.configuration_groups.parcel import Global, _ParcelBase, _ParcelEntry, as_optional_global
+from catalystwan.api.configuration_groups.parcel import Global, _ParcelBase, _ParcelEntry
 
 
 class ScalableGroupTagEntry(_ParcelEntry):
-    sgt_name: Optional[Global[str]] = Field(default=None, validation_alias="sgtName", serialization_alias="sgtName")
-    tag: Optional[Global[str]] = Field(default=None)
+    sgt_name: Global[str] = Field(validation_alias="sgtName", serialization_alias="sgtName")
+    tag: Global[str]
 
 
 class ScalableGroupTagParcel(_ParcelBase):
     type_: Literal["security-scalablegrouptag"] = Field(default="security-scalablegrouptag")
     entries: List[ScalableGroupTagEntry] = Field(validation_alias=AliasPath("data", "entries"), default_factory=list)
 
-    def add_entry(self, sgt_name: Optional[str] = None, tag: Optional[str] = None):
+    def add_entry(self, sgt_name: str, tag: str):
         self.entries.append(
             ScalableGroupTagEntry(
-                sgt_name=as_optional_global(sgt_name),
-                tag=as_optional_global(tag),
+                sgt_name=Global[str](value=sgt_name),
+                tag=Global[str](value=tag),
             )
         )
