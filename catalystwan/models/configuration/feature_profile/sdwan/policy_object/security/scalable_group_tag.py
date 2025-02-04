bf43e063430a3460f0f6aa6deaa33d9bd4749ec6
Szymon Basan <sbasan@cisco.com>
2025-02-04 09:07:59 +0100

    fix sgt model config

--- catalystwan/models/configuration/feature_profile/sdwan/policy_object/security/scalable_group_tag.py
+++ catalystwan/models/configuration/feature_profile/sdwan/policy_object/security/scalable_group_tag.py
@@ -1,17 +1,19 @@
 # Copyright 2024 Cisco Systems, Inc. and its affiliates
 from typing import List, Literal
 
-from pydantic import AliasPath, Field
+from pydantic import AliasPath, ConfigDict, Field
 
 from catalystwan.api.configuration_groups.parcel import Global, _ParcelBase, _ParcelEntry
 
 
 class ScalableGroupTagEntry(_ParcelEntry):
+    model_config = ConfigDict(populate_by_name=True)
     sgt_name: Global[str] = Field(validation_alias="sgtName", serialization_alias="sgtName")
     tag: Global[str]
 
 
 class ScalableGroupTagParcel(_ParcelBase):
+    model_config = ConfigDict(populate_by_name=True)
     type_: Literal["security-scalablegrouptag"] = Field(default="security-scalablegrouptag", exclude=True)
     entries: List[ScalableGroupTagEntry] = Field(validation_alias=AliasPath("data", "entries"), default_factory=list)
 
