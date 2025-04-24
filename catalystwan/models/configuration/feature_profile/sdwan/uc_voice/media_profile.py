c9d181a746d36021aad47f829b88ad141371fa47
Szymon Basan <116343782+sbasan@users.noreply.github.com>
2025-04-24 19:35:34 +0200

    fix media profile model (#70)

--- catalystwan/models/configuration/feature_profile/sdwan/uc_voice/media_profile.py
+++ catalystwan/models/configuration/feature_profile/sdwan/uc_voice/media_profile.py
@@ -1,18 +1,10 @@
 # Copyright 2024 Cisco Systems, Inc. and its affiliates
-from typing import List, Literal, Optional, Union
+from typing import List, Literal, Union
 
-from pydantic import AliasPath, BaseModel, ConfigDict, Field
+from pydantic import AliasPath, ConfigDict, Field
 
 from catalystwan.api.configuration_groups.parcel import Default, Global, Variable, _ParcelBase
 from catalystwan.models.common import MpDtmf, MpVoiceCodec
-
-
-class Codec(BaseModel):
-    model_config = ConfigDict(populate_by_name=True)
-    value: Union[Variable, List[MpVoiceCodec]] = Field()
-    pref_num: Optional[str] = Field(
-        default=None, validation_alias="prefNum", serialization_alias="prefNum", description="Preference number"
-    )
 
 
 class MediaProfileParcel(_ParcelBase):
