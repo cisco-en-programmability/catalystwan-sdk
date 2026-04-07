6eff48497cf7f6083d9e2b6f2e90813da52b1859
Szymon <sbasan@cisco.com>
2026-04-07 11:41:42 +0200

    introduce additional properties to DeviceVariable

--- catalystwan/api/templates/device_variable.py
+++ catalystwan/api/templates/device_variable.py
@@ -1,7 +1,13 @@
 # Copyright 2023 Cisco Systems, Inc. and its affiliates
 
-from pydantic import BaseModel
+from typing import Literal, Optional, Tuple
+
+from pydantic import BaseModel, PrivateAttr
+
+VariableObjectType = Literal["variable", "variableName"]
 
 
 class DeviceVariable(BaseModel):
     name: str
+    _object_type: VariableObjectType = PrivateAttr(default="variableName")
+    _template_path: Optional[Tuple[str, ...]] = PrivateAttr(default=None)
